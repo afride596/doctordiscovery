dc27390f66c26e347b491d63b85bb74d46f540a5
AFRIDE <afride596@gmail.com>
2024-11-27 23:45:08 +0530

    implemented  back to search Functionality

--- src/components/SearchResultsPage.jsx
+++ src/components/SearchResultsPage.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import { fetchDoctor } from "../utils/fetchDoctor";
 import PersonRoundedIcon from "@mui/icons-material/PersonRounded";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
 
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
 import LocationOnIcon from "@mui/icons-material/LocationOn";
 
 import { useQuery } from "@tanstack/react-query";
+import { Link } from "react-router";
 
 const SearchResultsPage = () => {
   const { data, error, isLoading } = useQuery({
@@ -26,12 +28,20 @@ const SearchResultsPage = () => {
     queueStatus,
     ratings,
     specialty,
-  } = data[1];
+  } = data;
   console.log(data);
 
   return (
-    <div className=" h-[100vh] flex-col w-[100vw] bg-[#f7fee7] flex items-center">
-      <div className="flex justify-center relative top-11  items-center gap-[450px]  w-[50vw] h-[20vh] ">
+    <div className=" h-[100%] flex-col w-[100%] bg-[#f7fee7] flex items-center ">
+      <div className="relative right-[370px] top-9">
+        <Link to="/">
+          <button className="border px-2 flex gap-2 py-2 rounded-md bg-white font-medium shadow-md">
+            <KeyboardBackspaceIcon />
+            Back to Search
+          </button>
+        </Link>
+      </div>
+      <div className="flex justify-center relative top-10  items-center gap-[450px]     w-[50vw] h-[20vh] ">
         <div className="">
           <h1 className="font-semibold text-2xl">
             Doctors near Current Location
@@ -47,43 +57,45 @@ const SearchResultsPage = () => {
         </div>
       </div>
       {/* Search Results */}
-      <div className=" border w-[50vw] gap-[200px] justify-center items-center px-16 py-10 flex bg-white rounded-md shadow-xl">
-        <div>
-          <h1 className="font-semibold text-xl">{name}</h1>
-          <p>{specialty}</p>
+      {data.map((doctor) => (
+        <div className=" border w-[50vw] mb-4 gap-[200px] justify-center items-center px-16  py-10 flex bg-white rounded-md shadow-xl">
           <div>
-            <span className="flex gap-1 text-base text-[#5f5656]  w-96 mt-2 mb-1">
-              <LocationOnIcon />
-              {location} ({distance} km away)
-            </span>
+            <h1 className="font-semibold text-xl">{doctor.name}</h1>
+            <p>{doctor.specialty}</p>
+            <div>
+              <span className="flex gap-1 text-base text-[#5f5656]  w-96 mt-2 mb-1">
+                <LocationOnIcon />
+                {doctor.location} ({doctor.distance} km away)
+              </span>
+            </div>
+            <div>
+              <span className="flex gap-1 text-base text-[#5f5656]  w-96 mt-2 mb-1">
+                <PersonRoundedIcon />
+                current queue:{doctor.queueStatus} patients
+              </span>
+            </div>
+            <div>
+              <span className="flex gap-1 text-base text-[#5f5656]  w-96 mt-2 mb-1">
+                <AccessTimeIcon />
+                Estimated Wait:{doctor.estimatedWaitTime}
+              </span>
+            </div>
           </div>
-          <div>
-            <span className="flex gap-1 text-base text-[#5f5656]  w-96 mt-2 mb-1">
-              <PersonRoundedIcon />
-              current queue:{queueStatus} patients
-            </span>
-          </div>
-          <div>
-            <span className="flex gap-1 text-base text-[#5f5656]  w-96 mt-2 mb-1">
-              <AccessTimeIcon />
-              Estimated Wait:{estimatedWaitTime}
-            </span>
+          <div className="flex flex-col gap-2">
+            <div>
+              <h1 className="font-semibold text-xl">Available Today</h1>
+            </div>
+            <div className="relative left-[76px]">
+              <span className="">{doctor.availableSlots[0]}</span>
+            </div>
+            <div className="mt-5">
+              <button className="border font-medium bg-yellow-300 rounded-lg shadow-lg px-3 py-2">
+                Book Appointment
+              </button>
+            </div>
           </div>
         </div>
-        <div className="flex flex-col gap-2">
-          <div>
-            <h1 className="font-semibold text-xl">Available Today</h1>
-          </div>
-          <div className="relative left-[76px]">
-            <span className="">{availableSlots[0]}</span>
-          </div>
-          <div className="mt-5">
-            <button className="border font-medium bg-yellow-300 rounded-lg shadow-lg px-3 py-2">
-              Book Appointment
-            </button>
-          </div>
-        </div>
-      </div>
+      ))}
     </div>
   );
 };
